87c8371da8b0e80b26b7dd790b36db29b31c4928
tomoyasakamoto32 <oregairu0302@gmail.com>
2021-02-28 23:32:29 +0900

    reactルーター記述

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+import { Restaurants } from './containers/Restaurants.jsx';
+import { Foods } from './containers/Foods.jsx';
+import { Orders } from './containers/Orders.jsx';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          テキストを変更してみる
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route
+        exact
+        path="/restaurants">
+        <Restaurants />
+        </Route>
+        <Route
+          exact
+          path="/restaurants/:restaurantsId/foods"
+          render={({ match }) =>
+            <Foods
+            match={match}
+            />
+          }
+        />
+        <Foods />
+        <Route
+          exact
+          path="/orders">
+          <Orders />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
