9c0dbc5c77348c9751351f4950ba86ef9f656309
tomoyasakamoto32 <oregairu0302@gmail.com>
2021-03-06 22:03:32 +0900

    reducerを定義

--- frontend/src/containers/Orders.jsx
+++ frontend/src/containers/Orders.jsx
@@ -1,17 +1,31 @@
 // --- 次でuseEffectを追加 ---
-import React, { Fragment, useEffect } from 'react';
+import React, { Fragment, useEffect, useReducer } from 'react';
 
-// --- ここから追加 ---
 import { fetchLineFoods } from '../apis/line_foods';
+// --- ここから追加 ---
+import {
+  initialState,
+  lineFoodsActionTypes,
+  lineFoodsReducer,
+} from '../reducers/lineFoods';
 // --- ここまで追加 ---
 
 export const Orders = () => {
+  // --- ここから追加 ---
+  const [state, dispatch] = useReducer(lineFoodsReducer, initialState);
+  // --- ここまで追加 ---
 
 // --- ここから追加 ---
   useEffect(() => {
+    dispatch({ type: lineFoodsActionTypes.FETCHING });
     fetchLineFoods()
       .then((data) =>
-        console.log(data)
+        dispatch({
+          type: lineFoodsActionTypes.FETCH_SUCCESS,
+          payload: {
+            lineFoodsSummary: data
+          }
+        })
       )
       .catch((e) => console.error(e));
   }, []);
